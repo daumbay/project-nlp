08baa83e69c7f4e799092769dd4e4e57da98d3e6
Aditya Matsa <aditya.matsa@icloud.com>
2024-05-27 17:12:41 +0530

    Imported all .scss files

--- starter_project/src/client/index.js
+++ starter_project/src/client/index.js
@@ -11,3 +11,8 @@ alert("I EXIST")
 // console.log("CHANGE!!");
 
 // sass files
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
